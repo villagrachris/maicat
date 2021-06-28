d77a62bb06e80853363295bececf5d7d3d9948cb
villagrachris <villagrachris@gmail.com>
2021-06-28 20:29:44 -0300

    ok

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,7 +12,7 @@ Consumo=Gasto_Carniceria+Gasto_Almacen+Gasto_Verduleria
 
 print("Estimado",Nombre,"Su consumo en pesos es de:",Consumo)
 
-print("Ingrese su numero de Tarjeta")
+print("Ingrese su numero de Tarjeta de credito")
 
 num_tarjeta=int(input())
 
