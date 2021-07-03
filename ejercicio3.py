5becb57e8bb8024d91e8ee9e2970021aaf457bf8
villagrachris <villagrachris@gmail.com>
2021-07-03 20:38:12 -0300

    ok

--- ejercicio3.py
+++ ejercicio3.py
@@ -14,6 +14,45 @@ if marca!="fiat" and marca!="chevrolet" and marca!="ford":
         marca=input("Por favor ingresar una nueva marca: ").lower()
         if marca!="fiat" and marca!="chevrolet" and marca!="ford":
            print("Usted ingreso una marca que no tenemos a la venta")
-           exit() 
+           exit()
+puertas=int(input("Ingrese la cantidad de puertas del vehiculo que desea comprar:"))
+
+if puertas!=3 and puertas!=5:
+    puertas=int(input(" Solo disponemos de autos con 3 y 5 puertas cual desea comprar:"))
+    if puertas!=3 and puertas!=5:
+      print("No disponemos del vehiculo de su preferencia de puertas, Gracias por su visita!")
+      exit()
+
+if marca=="ford" and monto==1500000:
+  print("Disponemos del modelo Ford K para su preferencia de compra")
+  respuesta=input("Desea realizar la compra del vehiculo que le ofrecemos SI o NO:").lower()
+  if respuesta=="si":
+     print("Espere el llamado de un representante de ventas, Gracias por su visita")
+     exit()
+  else:
+     print("Lo esperamos en otra oportunidad, gracias por su visita")
+     exit()   
+elif marca=="fiat" and (puertas==3 or puertas==5):
+  print("Disponemos del modelo Punto para su preferencia de compra")
+  respuesta=input("Desea realizar la compra del vehiculo que le ofrecemos SI o NO:").lower()
+  if respuesta=="si":
+     print("Espere el llamado de un representante de ventas, Gracias por su visita")
+     exit()
+  else:
+     print("Lo esperamos en otra oportunidad, gracias por su visita")
+     exit()
+elif marca=="chevrolet" and puertas==5 and monto>2000000:
+  print("Disponemos del modelo Zafira para su preferencia de compra")
+  respuesta=input("Desea realizar la compra del vehiculo que le ofrecemos SI o NO:").lower()
+  if respuesta=="si":
+     print("Espere el llamado de un representante de ventas, Gracias por su visita")
+     exit()
+  else:
+     print("Lo esperamos en otra oportunidad, gracias por su visita")
+     exit()
+else:
+  print ("Le agradecemos la visita a nuestro sitio de compras y los esperamos para su proxima compra, Gracias!")   
+
+
 
 
