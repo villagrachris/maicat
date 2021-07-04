8f74649c0208e32d9c5f5489c20d85af4c39d539
villagrachris <villagrachris@gmail.com>
2021-07-03 21:09:15 -0300

    ok

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+
+def compras_electro():
+       print("Disponemos de una heladera de $60000, un lavarropas de $45000 o una cocina de $25000")
+     
+
+
+print("Bienvenidos a Garbarino Compras")
+ingreso=int(input("1. Para la compra de electrodomesticos.2 Audio/TV.3 Ambos productos"))
+    if ingreso==1:
+      compras_electro()
+    
+
