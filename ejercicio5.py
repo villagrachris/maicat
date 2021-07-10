976479ca0f0ca49643fdf6c4ad755862b65243e3
villagrachris <villagrachris@gmail.com>
2021-07-10 14:55:03 -0300

    Ejercicio de Listas

---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+#Listas Ejericio de Python
+infantiles=["Blanca Nieve","Los Tres Chanchitos","Cenicienta"]
+novelas=["Don Quijote","La Novicia Rebelde"]
+policiales=["Sherlok Holmes","Muerte en el Nilo"]
+
+print(infantiles)
+
+
