9e355a4f46cc9024b69f1f1048bc882f6f75c363
villagrachris <villagrachris@gmail.com>
2021-07-09 22:20:15 -0300

    avance

--- ejercicio4.py
+++ ejercicio4.py
@@ -44,28 +44,31 @@ def facturacion():
           print("Posee un descuento de $5000 pesos del total de su compra, se debitará de su tarjeta",descuento)
           print("Gracias por su compra")
           exit() 
-      else:
-        print("El código ingresado no posee ningún descuento disponible, se debitará de su tarjeta",compra)
-        print("Gracias por su compra")
-        exit()   
+         else:
+          print("El código ingresado no posee ningún descuento disponible, se debitará de su tarjeta",compra)
+          print("Gracias por su compra")
+          exit()   
    else:
-    num_tarjeta=input("ingreso un numero inferior a 12 digitos de su tarjeta de credito, por reingrese los numeros")
-    num_tarjeta=input("Ingrese los 12 digitos de la tarjeta de credito")    
+    print("ingreso un numero inferior a 12 digitos de su tarjeta de credito, por reingrese los numeros")
+    facturacion()
+      
             
 print("Bienvenidos a Garbarino Compras")
 ingreso=int(input("1. Para la compra de electrodomesticos.2 Audio/TV.3 Ambos productos"))
-primer_compra=0
-segunda_compra=0
+primer_compra=0.0
+segunda_compra=0.0
+tercer_compra=0.0
 if ingreso==1:
  primer_compra=compra_electro()
 elif ingreso==2:
  segunda_compra=compra_tvaudio()     
 elif ingreso==3:
-  opcion=int(input("Si desea Electrodomesticos indique 1 en caso que sea TV/audio indique2"))
+  opcion=int(input("Si desea Electrodomesticos indique 1 en caso que sea TV/audio indique 2"))
   if opcion==1:
-     primer_compra=compra_electro()
+     tercer_compra=compra_electro()
+     
   elif opcion==2:
-     segunda_compra=compra_tvaudio()
+     tercer_compra=compra_tvaudio()
 else:
    print("No ingreso ninguna de las opciones sugeridas, gracias por su visita")
    exit()
