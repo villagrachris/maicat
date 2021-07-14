050a4c315c27db5eacff64af2461e8faa6657d4f
villagrachris <villagrachris@gmail.com>
2021-07-14 14:38:36 -0300

    ok

---
+++ funcione1.py
@@ -0,0 +1,14 @@
+jugadora=[27, 14, 20, 4, 30, 22, 3, 29, 8, 12, 5]
+jugadorb=[20, 25, 1, 6, 15, 26, 23, 17, 27, 0, 9, 11]
+
+def compra_propiedad (jugadorb,jugadora):
+  compra = 0
+  for numero in jugadora:
+    if numero not in jugadorb:
+      compra += 1
+  
+  print(compra)
+
+compra=0
+
+compra_propiedad(jugadorb,jugadora)
