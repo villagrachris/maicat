c9b01e5fdc4243c0586cf54640d88ec0f9d151b0
villagrachris <villagrachris@gmail.com>
2021-07-21 22:44:36 -0300

    ok

---
+++ reto2.py
@@ -0,0 +1,10 @@
+letra1=chr(77)
+letra2=chr(101)
+print(letra1,letra2)
+
+mensaje="771013210311711511697115321099711532113117101321081111153211211711011610111411111546"
+
+valor=mensaje.split("32")
+decifrar=str.isascii(mensaje)
+
+print(valor)
