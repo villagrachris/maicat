d020a8d26f62c2910f2478c1d1262dde77ae9f48
villagrachris <villagrachris@gmail.com>
2021-07-24 00:32:19 -0300

    ok

--- reto2.py
+++ reto2.py
@@ -2,13 +2,20 @@
 mensaje="771013210311711511697115321099711532113117101321081111153211211711011610111411111546"
 
 lista=mensaje.split("32")
+
+print(lista)
+
 i=0
+
 while i< len(lista):
-    
-    cod=[ele for sub in lista[i] for ele in sub]
-    
-    print(cod)
-      
+    palabra=str.split(lista[i])
+    silaba=str(palabra) 
+    if silaba[2]!="1":
+     print(chr(int(silaba[2]+silaba[3])))
+    elif silaba[2]=="1":
+     print(chr(int(silaba[2]+silaba[3]+silaba[4])))    
     i=i+1
+
+
 
    
